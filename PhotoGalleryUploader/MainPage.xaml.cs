928c84ccd475ef9d95810db4fcfdb4714202ae78
Moshe Tenenbaum <moshe.tenenbaum@gmail.com>
2018-10-17 11:49:51 -0400

    Some Refactoring

--- PhotoGalleryUploader/MainPage.xaml.cs
+++ PhotoGalleryUploader/MainPage.xaml.cs
@@ -37,46 +37,10 @@ namespace PhotoGalleryUploader
             DataContextChanged += (sender, events) =>
             {
                 ViewModel = DataContext as GalleryViewModel;
-                ViewModel.PropertyChanged -= OnPropertyChanged;
-                ViewModel.PropertyChanged += OnPropertyChanged;
-                
+
+
             };
 
-        }
-
-
-
-        //could be broken down better
-        private async Task LoadThumbnailsFromFiles(int pageCount = 20)
-        {
-
-            ViewModel.LocalIsBusy = true;
-            List<BitmapImage> images = new List<BitmapImage>();
-            foreach (var file in ViewModel.SelectedFiles)
-            {
-                var thumbnail = await file.GetThumbnailAsync(ThumbnailMode.PicturesView);
-                var image = new BitmapImage();
-                image.SetSource(thumbnail);
-                images.Add(image);
-
-            }
-            //var items = new Items(images);
-            ImageCollection = new ObservableCollection<BitmapImage>(images);
-            ViewModel.LocalIsBusy = false;
-        }
-
-        private ObservableCollection<BitmapImage> ImageCollection;
-
-        private async void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            switch (e.PropertyName)
-            {
-                case nameof(ViewModel.SelectedFiles):
-
-                    await LoadThumbnailsFromFiles(20);
-
-                    break;
-            }
         }
 
         private async void OpenFolderButtonClicked(object sender, RoutedEventArgs e)
@@ -86,9 +50,6 @@ namespace PhotoGalleryUploader
 
             if (selectedFolder != null)
             {
-
-
-
                 //    // Application now has read/write access to all contents in the picked folder
                 //    // (including other sub-folder contents)
                 ViewModel.FolderPath = selectedFolder.Path;
@@ -96,8 +57,6 @@ namespace PhotoGalleryUploader
                 futureAccessList.AddOrReplace("PickedImageFolder", selectedFolder, ViewModel.FolderPath);
 
                 var options = new QueryOptions(CommonFileQuery.DefaultQuery, FileExtensions.Image);
-                options.FolderDepth = FolderDepth.Deep;
-                
 
 
                 if (selectedFolder.AreQueryOptionsSupported(options))
@@ -108,7 +67,7 @@ namespace PhotoGalleryUploader
                     var k = selectedFolder.CreateFileQueryWithOptions(options);
                     var result = await k.GetFilesAsync(index, stepSize);
 
-                    ViewModel.SelectedFiles = new ObservableCollection<StorageFile>(result);                 
+                    ViewModel.SelectedFiles = new ObservableCollection<StorageFile>(result);
 
                 }
             }
@@ -116,28 +75,6 @@ namespace PhotoGalleryUploader
 
         }
     }
-
-    #region MaybeCode
-    public class Items : IIncrementalSource<StorageItemThumbnail>
-    {
-
-        public List<StorageItemThumbnail> images { get; set; }
-
-        public Items() : this(new List<StorageItemThumbnail>())
-        {
-
-        }
-
-        public Items(List<StorageItemThumbnail> list)
-        {
-            images = list;
-        }
-
-        public async Task<IEnumerable<StorageItemThumbnail>> GetPagedItemsAsync(int pageIndex, int pageSize, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            return images.GetRange(pageIndex, pageSize);
-            
-        }
-    }
-    #endregion
 }
+
+
